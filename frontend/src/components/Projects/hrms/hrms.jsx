f3c3a88cdd0e864ddb0c2aa7ca7997226365236f
yhun <jhunguidef@gmail.com>
2025-09-07 10:12:10 +0800

    I add a responsive layout for my boxes

--- frontend/src/components/Projects/hrms/hrms.jsx
+++ frontend/src/components/Projects/hrms/hrms.jsx
@@ -11,37 +11,44 @@ function hrms(){
     return(
         <> 
                      
-            <div className="projects_box projects_main_wrapper hrms_project">
+            <section className="projects_box projects_main_wrapper hrms_project">
 
-                <ul className="pname_box hrm_box">  
+                <ul className="box pname_box hrm_box">  
 
-                    <div className="hrms pcontent_frame hrms_header">
+                    <div className="hrms_grid hrms_header">
                         header
                     </div>
-                    
-                    <div className="hrms pcontent_frame screen_frame hrms_screen">
+
+
+
+                    <ol class="hrms_grid project_content_frame">
+                            
+                        <div className="pcontent_frame screen_frame hrms_screen">
                             <img src={front_a}></img>
-                    </div>
+                        </div>
 
-                    <div className="hrms pcontent_frame context_frame hrms_context">
+                        <div className=" pcontent_frame context_frame hrms_context">
 
-                        <span>Human Resource Management System</span>
-                        <p>
-                            The Human Resource Management System is a school project developed for improving 
-                            efficieny on managing employee data and jobposting process. The system allows HR to manage 
-                            employee records, recruitment workflows and jobposting process, while jobseekers has their own 
-                            platform that helps navigating jobs and company postions easily. HR can provide detailed jobposts for required positions.
-                        </p>
+                            <span>Human Resource Management System</span>
+                            <p>
+                                The Human Resource Management System is a school project developed for improving 
+                                efficieny on managing employee data and jobposting process. The system allows HR to manage 
+                                employee records, recruitment workflows and jobposting process, while jobseekers has their own 
+                                platform that helps navigating jobs and company postions easily. HR can provide detailed jobposts for required positions.
+                            </p>
 
-                        <ol class="tech_stack_div"> 
+                        </div>
+
+                        <ol class="hrms_grid tech_stack_div"> 
                             tech_stack_div                          
                         </ol>
 
-                    </div>
-         
+                    </ol>
+                
+
                 </ul>
                 
-            </div>
+            </section>
         </>
 
     )
--- frontend/src/components/Projects/projects.jsx
+++ frontend/src/components/Projects/projects.jsx
@@ -4,9 +4,6 @@ import Project_Cat from './project_cat/project_cat.jsx'
 import Hrms from './hrms/hrms.jsx'
 import Ptrs from './ptrs/ptrs.jsx'
 
-
-import useScrollAnimation from "../../hooks/userscroll_animation.jsx";
-
 function Projects(){
   
     return(
